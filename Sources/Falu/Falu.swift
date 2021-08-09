83474b1a16ae016d8351615a321668861d940b24
Seth Onyango <sethi.onyango@gmail.com>
2021-08-09 11:32:30 +0300

    Make request to initiate a payment

--- Sources/Falu/Falu.swift
+++ Sources/Falu/Falu.swift
@@ -1,5 +1,12 @@
-struct Falu {
-    var text = "Hello, World!"
+import TingleApiClient
+/**
+ * Entry-point to the Falu SDK
+ *
+ * Supports asynchronous methods to access the following Falu APIs:
+ *
+ * * [Evaluations API] [Create Evaluation] - Create an evaluation
+ */
+public class Falu {
     private let apiClient: FaluApiClient
     
     /**
@@ -12,4 +19,38 @@ struct Falu {
         self.apiClient = FaluApiClient(key)
     }
     
+    
+    /**
+     * Create a payment
+     *
+     * See [Create a payment](https://api.falu.io/v1/payments).
+     * `POST /v1/payments`
+     *
+     * - Parameter  request [The payment request object](https://falu.io)
+     * - Parameter  completion to receive the result or error
+     *
+     */
+    public func createPayment(request: PaymentRequest, _ completion: @escaping  (Result<Payment, FaluError>) -> Void){
+        apiClient.createPayment(paymentRequest: request) { (response, error) in
+            
+            if (error != nil) {
+                // show a network error here
+                completion(.failure(error as! FaluError))
+                return
+            }
+            
+            guard let response = response else {
+                completion(.failure(.Unknown))
+                return
+            }
+            
+            if response.successful && response.resource != nil {
+                completion(.success(response.resource!))
+                return
+            }
+            
+            let problem = response.problem
+            completion(.failure(.ApiException(statusCode:response.statusCode, errorCode: problem?.error_code)))
+        }
+    }
 }
