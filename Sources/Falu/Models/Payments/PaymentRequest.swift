af33e8b58833b12c31a0b8265d5aa2f7c920bb13
Seth Onyango <sethi.onyango@gmail.com>
2021-08-09 08:49:35 +0300

    Add payment request objects

---
+++ Sources/Falu/Models/Payments/PaymentRequest.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+
+/**
+ * [The payment request object](https://falu.io)
+ *
+ */
+struct PaymentRequest: Codable {
+    /**
+     * Amount of the payment in smallest currency unit.
+     */
+    var amount: Int
+    
+    /**
+     * Three-letter ISO currency code, in lowercase.
+     */
+    var currency: String
+    
+    /**
+     * The shortcode of the receiver. When not provided, it defaults to the default recipient.
+     * When not provided, either the default incoming business code or the first business code for the workspace is used depending on the kind.
+     * This value is usually required and different from the business short code when using BuyGood
+     */
+    var mpesa: MpesaPaymentRequest?
+}
