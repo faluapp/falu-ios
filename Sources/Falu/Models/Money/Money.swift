7bc3f6b7e66f04ddfe40c5ad0cdc217c64ace30a
Seth Onyango <sethi.onyango@gmail.com>
2021-08-11 14:04:56 +0300

    Determine the smallest unit of an amount based on the currency

--- Sources/Falu/Models/Money/Money.swift
+++ Sources/Falu/Models/Money/Money.swift
@@ -1,8 +1,23 @@
-//
-//  File.swift
-//  
-//
-//  Created by Seth Onyango on 11/08/2021.
-//
-
 import Foundation
+
+/**
+ * Implementation of Martin Fowler Money Design Pattern
+
+ */
+internal struct Money{
+    let amount:  Decimal
+    let currency: Currency
+    
+    init(amount: Decimal, currencyCode: String) {
+        self.currency = Currency(isoCurrencyCode: currencyCode.uppercased())
+        self.amount = amount
+    }
+}
+
+/// Minor Unit is a fraction of the base (ex. cents, stotinka, etc.)
+extension Money{
+    
+    public var amountInMinorUnits: Int {
+        return NSDecimalNumber(decimal: amount * pow(10, currency.minorUnits)).intValue
+    }
+}
